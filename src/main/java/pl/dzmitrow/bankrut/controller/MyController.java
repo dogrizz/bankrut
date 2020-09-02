2ca44b66188544b3c7ba25f82c04e3d4edc8ea6a
Dominik Zmitrowicz <dogrizz@gmail.com>
2020-09-02 19:36:51 +0200

    Basic restcontroller

---
+++ src/main/java/pl/dzmitrow/bankrut/controller/MyController.java
@@ -0,0 +1,14 @@
+package pl.dzmitrow.bankrut.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
+
+}
