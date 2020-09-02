db8fa4e9d20eaafc7c98c21a37f377a7b25e087e
Dominik Zmitrowicz <dogrizz@gmail.com>
2020-09-02 18:28:26 +0200

    Init

---
+++ src/main/java/pl/dzmitrow/bankrut/BankrutApplication.java
@@ -0,0 +1,13 @@
+package pl.dzmitrow.bankrut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankrutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankrutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dzmitrow/bankrut/BankrutApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dzmitrow.bankrut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankrutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
