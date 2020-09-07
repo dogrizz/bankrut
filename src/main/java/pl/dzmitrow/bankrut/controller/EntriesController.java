112479ce723a5900b1af37e01181fe44118a0067
Dominik Zmitrowicz <dogrizz@gmail.com>
2020-09-07 02:35:01 +0200

    Fix entries mapping

--- src/main/java/pl/dzmitrow/bankrut/controller/EntriesController.java
+++ src/main/java/pl/dzmitrow/bankrut/controller/EntriesController.java
@@ -1,16 +1,14 @@
 package pl.dzmitrow.bankrut.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.dzmitrow.bankrut.model.Entry;
 import pl.dzmitrow.bankrut.model.api.EntryDTO;
 import pl.dzmitrow.bankrut.producer.EntryProducer;
 
 import java.util.Collections;
 
-@RestController("/entries")
+@RestController
+@RequestMapping("/entries")
 public class EntriesController {
 
     private final EntryProducer entryProducer;
