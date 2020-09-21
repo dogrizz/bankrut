2e6f0b172402e5436d57db5902f70429b5cee23d
Dominik Zmitrowicz <dogrizz@gmail.com>
2020-09-21 17:01:10 +0200

    Add in memory user registration

--- src/main/java/pl/dzmitrow/bankrut/webcontroller/RegisterController.java
+++ src/main/java/pl/dzmitrow/bankrut/webcontroller/RegisterController.java
@@ -1,9 +1,11 @@
 package pl.dzmitrow.bankrut.webcontroller;
 
 import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.XSlf4j;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.dzmitrow.bankrut.model.api.RegisterUserDTO;
 
@@ -11,11 +13,23 @@ import pl.dzmitrow.bankrut.model.api.RegisterUserDTO;
 @Controller
 public class RegisterController {
 
-    public RegisterUserDTO registerUserDTO;
+    private UserDetailsService userDetailsService;
+
+    public RegisterController(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @RequestMapping(path = "/register/submit")
     public String register(RegisterUserDTO registerUserDTO){
         log.info("registering {}", registerUserDTO);
+
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username(registerUserDTO.getLogin())
+                        .password(registerUserDTO.getPassword())
+                        .roles("USER")
+                        .build();
+        ((InMemoryUserDetailsManager) userDetailsService).createUser(user);
         return "redirect:/login?registered";
     }
 
